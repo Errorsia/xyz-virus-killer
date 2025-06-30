4dfd36cf2350c2afbde742d8bbf9f81b6d952b01
Errorsia <Errorsia@outlook.com>
2025-06-30 13:08:02 +0800

    Beta 1.4

--- xyz-virus-killer.py
+++ xyz-virus-killer.py
@@ -1,27 +1,32 @@
 import os
-import tkinter as tk
 import time
+import tkinter as tk
+from tkinter import messagebox
+
+# from tkinter import columnspan
 
 """
 Update Log:
-Add the Log Module
-Add an Easter Egg
+Add Create & Read config module.
 
 更新日志:
-增添了日志模块
-增添了一个彩蛋
+
 
 Introduction:
 This project mainly depend on 5 main modules:
     Kill Virus, Rename Virus Files, Repair Files, Log and the GUI (window).
     The note of each function is below its name.
+
+Author's message:
+    I give up on this version.
+    And I won't fix any bugs of this version.
 """
 
-Version = "VIRUS KILLER V1.3"
+Version = "VIRUS KILLER V1.4"
 
 # Build log
-# Current condition: On
-build_Log = True
+# Condition: Messagebox return
+build_Log = None
 
 current_Log = None
 
@@ -61,7 +66,8 @@ def taskkill_Processes(process_Name):
         return False
     else:
         t.insert("end", "Error: Please tell developers!!\n")
-        write_Log(f"Unknown Error: Please tell developers!!")
+        tk.messagebox.showerror(title="error", message="Unknown Error: Please tell developers!!")
+        write_Log("Unknown Error: Please tell developers!!")
         return False
 
 
@@ -127,7 +133,7 @@ def clean_Button():
 
     # Easter_Egg module
     if Easter_Egg < 0:
-        None
+        nothing = None
     elif Easter_Egg < 4:
         Easter_Egg += 1
     else:
@@ -140,7 +146,7 @@ def clean_Button():
 
 def clean_Output():
     # t.delete("1.0", tk.END)
-    None
+    nothing = None
 
 
 def create_Log():
@@ -152,7 +158,7 @@ def create_Log():
         current_Log = f"log{current_time_stamp}"
         current_time = time.asctime()
 
-        with open(f"./log{current_time_stamp}.log.arthur", "x", encoding="utf-8") as file:
+        with open(f"./log{current_time_stamp}.log.arthur", "x", encoding="UTF-8") as file:
             file.write(f"{Version}\n")
             file.write(f"{current_Log}:\n")
             file.write(f"Current Work Path: {os.getcwd()}\n\n")
@@ -161,9 +167,8 @@ def create_Log():
             file.write("Operation Log:\n")
 
         # Create a bat to clean Logs
-        # Current condition: Off
-        if False:
-            with open(f"./Clean_Log.bat", "w", encoding="utf-8") as file:
+        if not os.path.exists("./Clean_Log.bat"):
+            with open(f"./Clean_Log.bat", "w", encoding="UTF-8") as file:
                 file.write(f"del /f /q *.arthur ")
 
 
@@ -172,8 +177,40 @@ def write_Log(log):
 
     global build_Log, current_Log
     if build_Log:
-        with open(f"./{current_Log}.log.arthur", "a", encoding="utf-8") as file:
+        with open(f"./{current_Log}.log.arthur", "a", encoding="UTF-8") as file:
             file.write(f"{log}\n")
+
+
+def log_configuration():
+    global build_Log
+
+    if os.path.exists("./VIRUS_KILLER_CONFIGURATION.arthur_config"):
+        with open(f"./VIRUS_KILLER_CONFIGURATION.arthur_config", "r", encoding="UTF-8") as file:
+            log_config_read = file.read()
+            log_enable = log_config_read[:1]
+
+            if log_enable == "1":
+                build_Log = True
+                create_Log()
+            else:
+                build_Log = False
+
+    else:
+        log_enable_bool = tk.messagebox.askokcancel(title="Save log or not",
+                                                    message="Do you want to save log?\n你想要保存日志吗?")
+        if log_enable_bool:
+            build_Log = True
+            log_config(1)
+            create_Log()
+        else:
+            build_Log = False
+            log_config(0)
+
+
+def log_config(log_config_value):
+    with open("./VIRUS_KILLER_CONFIGURATION.arthur_config", "x", encoding="UTF-8") as file:
+        file.write(f"{log_config_value};")
+    os.system(f"attrib +h {os.getcwd()}\\VIRUS_KILLER_CONFIGURATION.arthur_config")
 
 
 # Main Window (GUI)
@@ -184,37 +221,37 @@ window.geometry("1366x720")
 
 var = tk.StringVar()
 
-lable1 = tk.Label(window, textvariable=var, bg="cyan", width=45, font=("Arial", 40), height=2)
-lable1.pack()
+label1 = tk.Label(window, textvariable=var, bg="cyan", width=45, font=("Arial", 40), height=2)
+label1.grid(row=0, column=0, columnspan=2)
 
 button1 = tk.Button(window, text="Kill Viruses", font=30, width=40, height=2, command=Kill_Viruses)
-button1.pack()
+button1.grid(row=1, column=0)
 
 button2 = tk.Button(window, text="Fix What Viruses Make", font=30, width=40, height=2, command=fix_What_Viruses_Make)
-button2.pack()
+button2.grid(row=2, column=0)
 
 button3 = tk.Button(window, text="Auto Kill(Do #1 And #2)", font=30, width=40, height=2, command=Auto_Kill)
-button3.pack()
+button3.grid(row=1, column=1)
 
 button4 = tk.Button(window, text="Clean Screen", font=30, width=40, height=2, command=clean_Button)
-button4.pack()
+button4.grid(row=2, column=1)
 
 # button5 = tk.Button(window, text = "©", width = 1, height= 1, command= clean_Button)
 # button5.pack()
 
-lable2 = tk.Label(window, width=10, font=20, height=2)
-lable2.pack()
+label2 = tk.Label(window, width=10, font=20, height=2)
+label2.grid(row=3, column=0, columnspan=2)
 
 # text = "If you want to clean screen, press the Clean Screen button",
-lable3 = tk.Label(window, text="Output:", width=10, font=20, height=1)
-lable3.pack()
+label3 = tk.Label(window, text="Output:", width=10, font=20, height=1)
+label3.grid(row=4, column=0, columnspan=2)
 
-t = tk.Text(window, height=25)
-t.pack()
+t = tk.Text(window, height=30)
+t.grid(row=5, column=0, columnspan=2)
 
 var.set("VIRUS KILLER")
 
-create_Log()
+log_configuration()
 
 window.mainloop()
 
