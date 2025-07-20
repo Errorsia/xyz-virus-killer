8178f039350950cceae615b20c80c70c0d741899
Errorsia <Errorsia@outlook.com>
2025-07-20 18:07:00 +0800

    Add xyz_virus_killer_config

---
+++ xyz_virus_killer_config.py
@@ -0,0 +1,28 @@
+"""
+@author Errorsia <Errorsia@outlook.com>
+@author Ariskanyaa <Ariskanyaa@outlook.com>
+"""
+
+'''
+Code Name List:
+Kevin, 
+Elysia, 
+Aponia, 
+Eden, 
+Vill-V, 
+Kalpas, 
+Su, 
+Sakura, 
+Kosma, 
+Mobius, 
+Griseo, 
+Hua, 
+Pardofelis
+'''
+
+PROGRAMME_NAME = 'Virus Killer'
+VERSION = '3.0.0'
+FULL_VERSION = f'{PROGRAMME_NAME} V{VERSION}'
+INTERNAL_VERSION = '%03d%03d%03d' % (3, 0, 0)
+CODE_NAME = 'Hua'
+NICKNAME = 'Ego'
