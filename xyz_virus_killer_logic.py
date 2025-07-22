0dd3cf820ba254bd6152912ad51bb5c2144ba7eb
Errorsia <Errorsia@outlook.com>
2025-07-22 18:16:34 +0800

    Add xyz_virus_killer_logic

--- xyz_virus_killer_logic.py
+++ xyz_virus_killer_logic.py
@@ -13,3 +13,27 @@ import tkinter as tk
 from tkinter import messagebox
 import subprocess
 import xyzvk_v3_0_0_config as config
+
+
+class ErrorsiaVirusKillerLogic:
+    def __init__(self, gui):
+        self.gui = gui
+        self.logging = self.logger = self.handler = None
+
+        # Whether TSET ENVIRONMENT
+        # test = True
+
+        # Build log(Messagebox return)
+        # log_dictionary = {'build_Log': None}a
+        self.build_Log = None
+
+        self.disable_debug_frame = True
+
+        # Get the value of the environment variable %appdata%
+        self.appdata = os.getenv("APPDATA")
+        # appdata = os.path.expandvars("%APPDATA%")
+        self.file_directory = self.appdata + '/Arthur/VirusKiller'
+
+        # Whether show Easter Egg
+        # Current condition: On (If Easter_Egg < 0, it's Off)
+        self.Easter_Egg = 0
