fc490747f1843f4b6a842ae154ea5e8e31e059cd
Errorsia <Errorsia@outlook.com>
2025-07-22 18:16:08 +0800

    Add xyz_virus_killer_logic

---
+++ xyz_virus_killer_logic.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Author: Ariskanyaa <Ariskanyaa@outlook.com>
+# Author: Errorsia <Errorsia@outlook.com>
+# License: GPL v3
+
+"""
+Logic module for xyzvk
+"""
+
+import os
+import time
+import tkinter as tk
+from tkinter import messagebox
+import subprocess
+import xyzvk_v3_0_0_config as config
