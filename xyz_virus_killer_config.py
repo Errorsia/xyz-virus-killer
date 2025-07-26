9f84272bd868b6007951d7eec00ba4e017013c29
Errorsia <Errorsia@outlook.com>
2025-07-26 21:47:59 +0800

    Version 3.0.1 - config

--- xyz_virus_killer_config.py
+++ xyz_virus_killer_config.py
@@ -37,8 +37,8 @@ Config of xyzvk
 # Pardofelis
 
 PROGRAMME_NAME = 'Virus Killer'
-VERSION = '3.0.0'
+VERSION = '3.1.0'
 FULL_VERSION = f'{PROGRAMME_NAME} V{VERSION}'
-INTERNAL_VERSION = '%03d%03d%03d' % (3, 0, 0)
+INTERNAL_VERSION = '%03d%03d%03d' % (3, 1, 0)
 CODE_NAME = 'Hua'
 NICKNAME = 'Ego'
