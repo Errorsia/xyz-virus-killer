bff62782722a40715b791e9e728eb89fa8e01afc
Errorsia <Errorsia@outlook.com>
2025-07-26 21:47:08 +0800

    Version 3.0.1 - config

--- xyz_virus_killer_config.py
+++ xyz_virus_killer_config.py
@@ -1,24 +1,40 @@
+# -*- coding: utf-8 -*-
+# Authors:
+#   - Ariskanyaa <Ariskanyaa@outlook.com>
+#   - Errorsia <Errorsia@outlook.com>
+# License: GNU General Public License v3.0 or later (GPLv3+)
+# See: https://www.gnu.org/licenses/gpl-3.0.html
+# Copyright (C) 2024 Errorsia, Ariskanyaa
+#
+# This file is part of the xyzvk project and is distributed under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
 """
-@author Errorsia <Errorsia@outlook.com>
-@author Ariskanyaa <Ariskanyaa@outlook.com>
+Config of xyzvk
 """
 
-'''
-Code Name List:
-Kevin, 
-Elysia, 
-Aponia, 
-Eden, 
-Vill-V, 
-Kalpas, 
-Su, 
-Sakura, 
-Kosma, 
-Mobius, 
-Griseo, 
-Hua, 
-Pardofelis
-'''
+
+# Code Name List:
+# Kevin,
+# Elysia,
+# Aponia,
+# Eden,
+# Vill-V,
+# Kalpas,
+# Su,
+# Sakura,
+# Kosma,
+# Mobius,
+# Griseo,
+# Hua,
+# Pardofelis
 
 PROGRAMME_NAME = 'Virus Killer'
 VERSION = '3.0.0'
