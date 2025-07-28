c86f04eb6bff1610594ff3388ffc89daaccdeeee
Errorsia <Errorsia@outlook.com>
2025-07-28 09:49:54 +0800

    Alpha 3.1.0 - logic

--- xyz_virus_killer_logic.py
+++ xyz_virus_killer_logic.py
@@ -518,14 +518,15 @@ class ErrorsiaVirusKillerLogic:
         elif self.Easter_Egg < 4:
             self.Easter_Egg += 1
         else:
-            self.gui.var.set("Copyright © 2024 - 2030 Arthur_xyz.All Rights Reserved")
+            # self.gui.var.set("Copyright © 2024 - 2030 Arthur_xyz")
+            self.gui.var.set("Copyright (C) 2024 Arthur_xyz")
 
-            self.logger.debug('=' * 52)
-            self.logger.debug('Copyright 2024 - 2030 Arthur_xyz.All Rights Reserved')
+            self.logger.debug('=' * 37)
+            self.logger.debug('Copyright 2024 Arthur_xyz')
             self.logger.debug('The Easter Egg was discovered by you!')
             self.logger.debug('Developer:\tArthur_xyz')
             self.logger.debug('Email:\tArthur_xyz@outlook.com')
-            self.logger.debug('=' * 52)
+            self.logger.debug('=' * 37)
 
             self.Easter_Egg = 0
 
