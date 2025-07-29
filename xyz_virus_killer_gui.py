d23d973b90aeaf5b917bab58965d9350c5284315
Errorsia <Errorsia@outlook.com>
2025-07-29 10:48:27 +0800

    Update Version 3.1.0 - gui

--- xyz_virus_killer_gui.py
+++ xyz_virus_killer_gui.py
@@ -170,4 +170,3 @@ class ErrorsiaVirusKillerGUI:
     def debug_combobox_on_select(self, event):
         selected_value = self.debug_combobox1.get()
         self.logic.set_log_level(selected_value)
-
