b0b32c0c2aadbaad881ea03be750ddc25f26de8e
Errorsia <Errorsia@outlook.com>
2025-07-29 10:50:33 +0800

    Alpha 3.1.0 - main

--- xyz_virus_killer_main.py
+++ xyz_virus_killer_main.py
@@ -1,15 +1,46 @@
 # -*- coding: utf-8 -*-
-# Author: Ariskanyaa <Ariskanyaa@outlook.com>
-# Author: Errorsia <Errorsia@outlook.com>
-# License: GPL v3
+# Project Name: xyzvk
+# Version: 3.1.0
+# Authors:
+#   - Ariskanyaa <Ariskanyaa@outlook.com>
+#   - Errorsia <Errorsia@outlook.com>
+# License: GNU General Public License v3.0 or later (GPLv3+)
+# See: https://www.gnu.org/licenses/gpl-3.0.html
 
-# Version 3.0.0
+# Project Name: xyzvk
+# Copyright (C) 2024 Errorsia, Ariskanyaa
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+# THE PROGRAMME ONLY RUNS ON WINDOWS(NT) !
+# I don't think someone will run an EXE programme on Linux(except wine), MacOS etc.
+
 
 """
 Main module for xyzvk
-
-Copyright ©  2024 Errorsia.All Rights Reserved
 """
+
+# Update Log:
+# Rebuild all files
+#
+# 更新日志:
+# 重构所有文件
+
+# Author's message:
+#     Why the codes is more and more complex, while the lines are fewer and fewer?
+#     There is no bugs at present!
+#     But programme is still TESTING!
 
 import logging
 import os
